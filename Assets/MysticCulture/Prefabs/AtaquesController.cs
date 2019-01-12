8d7d09898ec4d57fb69095722aad83eb04b944cf
Andre <andrebe07@gmail.com>
2019-01-12 00:44:42 -0300

    Arco

--- Assets/MysticCulture/Prefabs/AtaquesController.cs
+++ Assets/MysticCulture/Prefabs/AtaquesController.cs
@@ -37,6 +37,7 @@ public class AtaquesController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         if (countA)
         {
             contadorA -= Time.deltaTime;
